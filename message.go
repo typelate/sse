a743f13fe3e8deaa7dd54195cee44508a7ff8b48
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2026-05-28 17:18:49 -0700

    inline constants

--- message.go
+++ message.go
@@ -17,24 +17,6 @@ import (
 	"time"
 )
 
-const (
-	// LastEventIDRequestHeaderKey is the header a client sets when
-	// reconnecting, carrying the id of the last event it received so the
-	// server can resume the stream.
-	LastEventIDRequestHeaderKey = "Last-Event-Id"
-
-	ContentTypeResponseHeaderKey   = "Content-Type"
-	ContentTypeResponseHeaderValue = "text/event-stream; charset=utf-8"
-
-	ConnectionResponseHeaderKey   = "Connection"
-	ConnectionResponseHeaderValue = "keep-alive"
-
-	// CacheControlResponseHeaderKey is set to no-cache so intermediaries
-	// do not buffer the stream.
-	CacheControlResponseHeaderKey   = "Cache-Control"
-	CacheControlResponseHeaderValue = "no-cache"
-)
-
 // ErrInvalidField is returned when an id, event, or comment value contains a
 // character the SSE wire format forbids: CR or LF (reserved as line
 // terminators) in any field, or a NUL in an id (which the client ignores,
@@ -64,15 +46,15 @@ func New(res http.ResponseWriter, req *http.Request, code int) (*Response, bool)
 		return nil, false
 	}
 	var lastEventID *string
-	canonical := textproto.CanonicalMIMEHeaderKey(LastEventIDRequestHeaderKey)
+	canonical := textproto.CanonicalMIMEHeaderKey("Last-Event-Id")
 	if values, isSet := req.Header[canonical]; isSet && len(values) > 0 && values[0] != "" {
 		v := values[0]
 		lastEventID = &v
 	}
 	h := res.Header()
-	h.Set(ContentTypeResponseHeaderKey, ContentTypeResponseHeaderValue)
-	h.Set(ConnectionResponseHeaderKey, ConnectionResponseHeaderValue)
-	h.Set(CacheControlResponseHeaderKey, CacheControlResponseHeaderValue)
+	h.Set("Content-Type", "text/event-stream; charset=utf-8")
+	h.Set("Connection", "keep-alive")
+	h.Set("Cache-Control", "no-cache")
 	rw.WriteHeader(code)
 	rw.Flush()
 	return &Response{res: rw, lastEventID: lastEventID}, true
